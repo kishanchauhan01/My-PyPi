61b0bc5ba55e6decf90276b0bbf5034126e69648
kishanchauhan01 <kishanchauhan2006.25@gmail.com>
2024-09-26 09:29:32 +0530

    add singlyCircularLL

--- DScollection_base/DScollection/SinglyCircularLL.py
+++ DScollection_base/DScollection/SinglyCircularLL.py
@@ -37,6 +37,79 @@ class SinglyCircularLL:
 
         else:
             self._tail._next = self._Node(val, self._head)
+            self._tail = self._tail._next
+            self._size += 1
+
+    def insertAtPos(self, pos, val):
+        if self.is_empty() or ((self._size == 1) and (pos > 1)):
+            print("Either list is empty or postition is greter than 1")
+
+        elif (self._size == 1) and (pos == 1):
+            self.insertAtTail(val)
+
+        else:
+            if pos > self._size:
+                print("Index out of range")
+            elif pos == self._size:
+                self.insertAtTail(val)
+
+            else:
+                count = 1
+                thead = self._head
+                while count < pos - 1:
+                    count += 1
+                    thead = thead._next
+
+                temp = self._Node(val, thead._next)
+                thead._next = temp
+                self._size += 1
+
+    def deleteHead(self):
+        if self.is_empty():
+            print("List is empty")
+
+        elif len(self) == 1:
+            self._head = None
+            self._tail = None
+            self._size -= 1
+
+        else:
+            self._head = self._head._next
+            self._tail._next = self._head
+            self._size -= 1
+
+    def deleteTail(self):
+        if self.is_empty():
+            print("List is empty")
+
+        else:
+            temp = self._head
+            while temp._next._next != self._head:
+                temp = temp._next
+            temp._next = self._head
+            self._tail = temp
+            self._size -= 1
+
+    def deleteAtPos(self, pos):
+        if self.is_empty():
+            print("List is empty")
+
+        elif pos > self._size:
+            print("Index out of range")
+
+        elif self._size == 1 and pos == 1:
+            self.deleteHead()
+
+        else:
+            count = 1
+            thead = self._head
+            while count < pos - 1:
+                count += 1
+                thead = thead._next
+            thead._next = thead._next._next
+            if thead._next == self._head:
+                self._tail = thead
+            self._size -= 1
 
     def traversse(self):
         if self.is_empty():
@@ -53,3 +126,38 @@ class SinglyCircularLL:
 
             if temp._next == self._head:
                 print(temp._element, "-> Null")
+
+    def get_head(self):
+        if self._head or self._tail == None:
+            print("List is empty")
+        else:
+            print(self._head._element)
+
+    def get_tail(self):
+        if self._head or self._tail == None:
+            print("List is empty")
+        else:
+            print(self._tail._element)
+
+
+c = SinglyCircularLL()
+c.insertAtHead(1)
+c.insertAtHead(2)
+c.insertAtHead(3)
+c.insertAtHead(4)
+c.insertAtHead(5)
+c.traversse()
+c.deleteHead()
+print(len(c))
+c.traversse()
+c.insertAtPos(2, 85)
+c.traversse()
+c.deleteAtPos(3)
+
+# c.insertAtHead(2)
+# c.insertAtHead(3)
+# c.insertAtHead(4)
+# c.insertAtTail(10)
+c.traversse()
+c.get_head()
+c.get_tail()
